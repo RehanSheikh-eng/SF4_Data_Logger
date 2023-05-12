c3779b42b8bb99e623e15ffad4a1313b56139e46
Rehan Sheikh <rs2126@cam.ac.uk>
2023-05-12 21:30:19 +0100

    Add unit tests for serial_audio module
    In this commit:
    - Added tests for the `setup_serial` function to verify that it correctly sets up a Serial connection with specified COM port and baud rate.
    - Implemented tests for `listen_for_audio` function to ensure it correctly captures audio data, handles start and end bytes, and yields the appropriate data chunks.

---
+++ python_code/test/test_serial_audio.py
@@ -0,0 +1,23 @@
+import unittest
+import serial
+from unittest.mock import MagicMock, patch
+from ..src.serial_audio import setup_serial, listen_for_audio
+
+
+class TestSerialAudio(unittest.TestCase):
+    @patch('serial.Serial')
+    def test_setup_serial(self, mock_serial):
+        com_port = 'COM4'
+        baud_rate = 9600
+        setup_serial(com_port, baud_rate)
+        mock_serial.assert_called_once_with(com_port, baud_rate)
+
+    def test_listen_for_audio(self):
+        ser = MagicMock(spec=serial.Serial)
+        ser.read.side_effect = [b'\x02', b'a', b'b', b'\x03', b'\x02', b'c', b'\x03']
+        gen = listen_for_audio(ser)
+        self.assertEqual(next(gen), bytearray(b'ab'))
+        self.assertEqual(next(gen), bytearray(b'c'))
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test.py
@@ -0,0 +1,6 @@
+from python_code.test import test_serial_audio
+import unittest
+
+
+
+unittest.main(test_serial_audio)
