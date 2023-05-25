21a9dacfc6ff760a4cb33b89e0ecef9b44777301
Rehan Sheikh <rs2126@cam.ac.uk>
2023-05-25 22:43:33 +0100

    Configured firebase realtime listeners

--- myapp/front_end/src/services/StoriesService.js
+++ myapp/front_end/src/services/StoriesService.js
@@ -1,20 +1,21 @@
-// StoriesService.js
 import { useState, useEffect } from 'react';
-import firebase from './firebase';
+import { collection, onSnapshot } from 'firebase/firestore';
+import db from './firebase';
 
 const useStoriesService = () => {
   const [stories, setStories] = useState([]);
 
   useEffect(() => {
-    const unsubscribe = firebase.firestore().collection('stories')
-      .onSnapshot(snapshot => {
-        const newStories = snapshot.docs.map(doc => ({
-          id: doc.id,
-          ...doc.data()
-        }));
+    const storiesCollection = collection(db, 'stories');
 
-        setStories(newStories);
-      });
+    const unsubscribe = onSnapshot(storiesCollection, (snapshot) => {
+      const newStories = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+
+      setStories(newStories);
+    });
 
     // Clean up the subscription on unmount
     return () => unsubscribe();
