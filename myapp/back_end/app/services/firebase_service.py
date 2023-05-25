4ba8b5b463e648fcadcce17f82e5c5703f557153
Rehan Sheikh <rs2126@cam.ac.uk>
2023-05-25 21:53:43 +0100

    Added GET stories endpoints

--- myapp/back_end/app/services/firebase_service.py
+++ myapp/back_end/app/services/firebase_service.py
@@ -1,12 +1,13 @@
-from firebase_admin import storage, firestore, initialize_app, credentials
+from firebase_admin import storage, firestore, initialize_app, credentials, _apps
 import uuid
 
 class FirebaseService:
     def __init__(self):
         self.cred = credentials.Certificate("C:/Users/relic/Documents/School/Engineering_Cam/Part IIA/SF4/SF4_Data_Logger/myapp/back_end/sf4-datalogger-firebase-adminsdk-fk860-d7a1a6c3c6.json")
-        initialize_app(self.cred, {
-            'storageBucket': 'sf4-datalogger.appspot.com',
-        })
+        if not _apps:
+            initialize_app(self.cred, {
+                'storageBucket': 'sf4-datalogger.appspot.com',
+            })
         self.db = firestore.client()
         self.bucket = storage.bucket()
 
@@ -20,3 +21,17 @@ class FirebaseService:
         doc_ref.set(data)
         return unique_id
 
+    def get_all_from_collection(self, collection_name):
+        docs = self.db.collection(collection_name).stream()
+        result = []
+        for doc in docs:
+            result.append(doc.to_dict())
+        return result
+        
+    def get_from_firestore(self, collection_name, document_id):
+        doc_ref = self.db.collection(collection_name).document(document_id)
+        doc = doc_ref.get()
+        if doc.exists:
+            return doc.to_dict()
+        else:
+            return None
