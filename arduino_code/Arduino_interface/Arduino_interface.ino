8afe12eb0eb44f27c15ed57bf3966562d9e017fe
Kyle Van Der Spuy <kv330@cam.ac.uk>
2023-05-30 13:46:42 +0100

    double digits

--- arduino_code/Arduino_interface/Arduino_interface.ino
+++ arduino_code/Arduino_interface/Arduino_interface.ino
@@ -70,68 +70,78 @@ void loop() {
 
       // Store Character
       CharIn = Serial.read();
-      if (CharIn == 207){
+      if (CharIn == 35){ // End of text signal
         digitalWrite(red,LOW); // Turn red LED OFF to indicate to user end of story
-          flagRead = 0; // Change state
+        flagRead = 0; // Change state
       }
-      else if (CharIn == 208){
+      else if (CharIn == 126){ // Word length has double digits signal
         DoubleDigits = 1;
       }
-      // If Character is a digit (equal to length of word) it marks start of word
-      else if (isdigit(CharIn)){
-        if (DoubleDigits == 1){
-          LineLen += CharIn - 38; // Subtract 38 (0 in bytes - 10) to get word length
-          DoubleDigits = 0;
-        }
-        else{
-          LineLen += CharIn - 48; // Subtract 48 (0 in bytes) to get word length
-        }
+      else{
+        // If Character is a digit (equal to length of word) it marks start of word
+        if (isdigit(CharIn)){
+          if (DoubleDigits == 1){
+            LineLen += CharIn - 38; // Subtract 38 (0 in bytes - 10) to get double digit word length
+          }
+          else{
+            LineLen += CharIn - 48; // Subtract 48 (0 in bytes) to get word length
+          }
           
-        // If word not at the beginning of the line need to add a space to separate from previous word
-        if (LineIndex > 0){
-          LineLen ++;
-        }
-
-        //If total line length greater than 16, out of display, need to move to new line
-        if (LineLen > 16){
-          LineLen = CharIn - 48; // Add word length to new line
-          LineIndex = 0; //Reset index to start text at beginning of line
-            
-          // If end of first line, need to start at beginning of second line
-          if (NewLine == 0){
-            NewLine = 1;
-            lcd.setCursor(0, 1);
+          // If word not at the beginning of the line need to add a space to separate from previous word
+          if (LineIndex > 0){
+            LineLen ++;
           }
 
-          // If end of second line, need to move second line to first line and start again at beginning of second line
-          else{
-            lcd.clear();
-
-            // Move second line to first line
-            lcd.setCursor(0, 0);
-            for (int i = 0; i < 16; i++){
-              lcd.print(LineTwo[i]);
-              LineOne[i] = LineTwo[i];
-              LineTwo[i] = 0x14;
+          //If total line length greater than 16, out of display, need to move to new line
+          if (LineLen > 16){
+            
+            // Add word length to new line
+            if (DoubleDigits == 1){
+              LineLen = CharIn - 38;
+            }
+            else{
+              LineLen = CharIn - 48;
+            }
+            LineIndex = 0; //Reset index to start text at beginning of line
+            
+            // If end of first line, need to start at beginning of second line
+            if (NewLine == 0){
+              NewLine = 1;
+              lcd.setCursor(0, 1);
             }
 
-            // Start at beginning of second line
-            lcd.setCursor(0, 1);
-          }
-        }
+            // If end of second line, need to move second line to first line and start again at beginning of second line
+            else{
+              lcd.clear();
 
-        // If not at the beginning of the line, need to add a space to separate from previous word
-        else if (LineIndex > 0){
-          CharIn = 0x14; // Space encoded by byte = 20
-          if (NewLine == 0){
-            LineOne[LineIndex] = CharIn;
+              // Move second line to first line
+              lcd.setCursor(0, 0);
+              for (int i = 0; i < 16; i++){
+                lcd.print(LineTwo[i]);
+                LineOne[i] = LineTwo[i];
+                LineTwo[i] = 0x14;
+              }
+
+              // Start at beginning of second line
+              lcd.setCursor(0, 1);
+            }
           }
-          else {
-            LineTwo[LineIndex] = CharIn;
+
+          // If not at the beginning of the line, need to add a space to separate from previous word
+          else if (LineIndex > 0){
+            CharIn = 0x14; // Space encoded by byte = 20
+            if (NewLine == 0){
+              LineOne[LineIndex] = CharIn;
+            }
+            else {
+              LineTwo[LineIndex] = CharIn;
+            }
+            lcd.print(CharIn);
+            LineIndex ++;
           }
-          lcd.print(CharIn);
-          LineIndex ++;
+          DoubleDigits = 0; // Reset word length to single digit after second digit read
         }
+      
         // The length of the word and fitting it within the line is already accounted for using the above
         // Just need to print word one character at a time
         else{
