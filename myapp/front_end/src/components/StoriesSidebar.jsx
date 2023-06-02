3559fcc1ff555adbe8b103245a657b1c2803a559
Rehan Sheikh <rs2126@cam.ac.uk>
2023-06-02 11:24:48 +0100

    Changed layout of sidebar

--- myapp/front_end/src/components/StoriesSidebar.jsx
+++ myapp/front_end/src/components/StoriesSidebar.jsx
@@ -1,19 +1,52 @@
-// StoriesSidebar.js
 import React from 'react';
-import { List, ListItem, ListItemText } from '@mui/material';
+import { List, ListItem, ListItemText, Button } from '@mui/material';
 import useStoriesService from '../services/StoriesService';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles({
+  listItem: {
+    marginBottom: '10px',
+    border: '1px solid #ddd',
+    borderRadius: '5px',
+    '&:hover': {
+      backgroundColor: '#f5f5f5',
+    },
+  },
+  list: {
+    maxHeight: '80vh',
+    overflowY: 'auto',
+    '&::-webkit-scrollbar': {
+      width: '0.5em',
+    },
+    '&::-webkit-scrollbar-thumb': {
+      backgroundColor: 'slategrey',
+      // outline: '1px solid slategrey'
+    }
+  },
+  button: {
+    marginTop: '10px',
+  },
+});
 
 const StoriesSidebar = ({ onStoryClick }) => {
+  const classes = useStyles();
   const stories = useStoriesService();
 
   return (
-    <List component="nav">
-      {stories.map(story => (
-        <ListItem button key={story.id} onClick={() => onStoryClick(story)}>
-          <ListItemText primary={`Story ID: ${story.transcription}`} />
-        </ListItem>
-      ))}
-    </List>
+    
+    <div>
+      <Button variant="contained" color="primary" className={classes.button}>
+        Add Story
+      </Button>
+      <List component="nav" className={classes.list}>
+        {stories.map(story => (
+          <ListItem button key={story.id} onClick={() => onStoryClick(story)} className={classes.listItem}>
+            <ListItemText primary={`${story.transcription}`} />
+          </ListItem>
+        ))}
+      </List>
+
+    </div>
   );
 };
 
