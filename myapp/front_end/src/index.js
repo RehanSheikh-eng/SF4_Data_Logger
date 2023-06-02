b6f86baafb8d3363c7f74acee83436581b1ca83b
Rehan Sheikh <rs2126@cam.ac.uk>
2023-06-02 11:24:20 +0100

    Added theme to index

--- myapp/front_end/src/index.js
+++ myapp/front_end/src/index.js
@@ -3,11 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#3f51b5', // replace with your desired primary color
+    },
+    secondary: {
+      main: '#f50057', // replace with your desired secondary color
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
